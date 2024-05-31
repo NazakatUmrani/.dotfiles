617043099ab86bc6e76916241df54268e1334194
Nazakat Umrani - NixOS Linux <unazakat70@gmail.com>
2024-05-31 16:54:30 +0500

    Qt5ct Environment Variable added

--- system/configuration.nix
+++ system/configuration.nix
@@ -102,6 +102,14 @@
     # WLR_NO_HARDWARE_CURSORS = "1";
     # Hint electron apps to use wayland
     NIXOS_OZONE_WL = "1";
+    #XDG_CURRENT_DESKTOP = "Hyprland";
+    #XDG_SESSION_DESKTOP = "Hyprland";
+    #XDG_SESSION_TYPE = "wayland";
+    #GDK_BACKEND = "wayland";
+    #GTK_USE_PORTAL = "1";
+    QT_QPA_PLATFORMTHEME = "qt5ct";
+    #QT_QPA_PLATFORM = "wayland";
+    #MOZ_ENABLE_WAYLAND = "1";
   };
 
   # Configure keymap in X11
