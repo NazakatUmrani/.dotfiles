af6396e52e8f74e06dbeb5321b3167adeaa209ba
Nazakat Umrani - NixOS Linux <unazakat70@gmail.com>
2024-06-04 02:19:34 +0500

    Wallpaper script modified

--- users/nazakat/hypr/wallpaperSetter.sh
+++ users/nazakat/hypr/wallpaperSetter.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-swww img /home/nazakat/Pictures/Wallpapers/rebellious_tower.png --transition-step 59 --transition-fps 30 --transition-type grow --transition-pos 1285,100 &
+swww img /home/nazakat/MyData/Wallpapers/gruvbox-mountain-village.png &
+# swww img /home/nazakat/Pictures/Wallpapers/rebellious_tower.png --transition-step 59 --transition-fps 30 --transition-type grow --transition-pos 1285,100 &
