3b8c5f1479d4644077dd539316774f561a5c924a
Nazakat Umrani - NixOS Linux <unazakat70@gmail.com>
2024-06-03 17:03:29 +0500

    Nvim reconfigured, LSP, LuaLine, TreeSitter and more

---
+++ users/nazakat/nvim/lua/options.lua
@@ -0,0 +1,20 @@
+-- assigning leade  key to space
+-- you can think of <leader> in keybinding as space
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.g.clipboard = 'unamedplus'
+
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.signcolumn = 'yes'
+
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+
+vim.o.updatetime = 300
+
+vim.termguicolors = true
+
+vim.o.mouse = 'a'
---
+++ users/nazakat/nvim/lua/pluginlist.lua
@@ -0,0 +1,69 @@
+return {
+
+    { 'numToStr/Comment.nvim', opts = {} },
+
+    {
+        "numToStr/Comment.nvim",
+        config = function()
+            require("Comment").setup()
+        end
+    },
+
+    {
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme gruvbox")
+        end
+    },
+    --
+    
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+
+    'folke/neodev.nvim', -- new
+
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+            'rafamadriz/friendly-snippets',
+            
+            'hrsh7th/cmp-nvim-lsp',
+            
+        },
+    },
+
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+    },
+
+
+    { 
+        'nvim-telescope/telescope.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim' } 
+    },
+
+    -- {
+    --     'nvim-telescope/telescope-fzf-native.nvim',
+    --     build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+    -- },
+
+
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = {
+            "nvim-tree/nvim-web-devicons"
+        },
+        config = function()
+            require("lualine").setup({
+                icons_enabled = true,
+                theme = 'onedark',
+            })
+        end,
+    }
+
+}
