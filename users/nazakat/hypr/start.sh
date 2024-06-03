27905da1fa76a7116d15d3e58f063ac77c11b150
Nazakat Umrani - NixOS Linux <unazakat70@gmail.com>
2024-06-04 04:18:19 +0500

    Waybar loads first [Start.sh]

--- users/nazakat/hypr/start.sh
+++ users/nazakat/hypr/start.sh
@@ -4,11 +4,11 @@
 swww-daemon --format xrgb &
 ~/.config/hypr/wallpaperSetter.sh init
 
-# Network Manager Applet
-nm-applet --indicator &
-
 # Add the bar
 waybar &
+
+# Network Manager Applet
+nm-applet --indicator &
 
 # dunst
 dunst
