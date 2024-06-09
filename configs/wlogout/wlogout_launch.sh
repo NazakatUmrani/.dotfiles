0e5a957fceaef5934ba1f65668f107b0e1cc88d0
Nazakat Umrani - NixOS Linux <unazakat70@gmail.com>
2024-06-10 02:20:46 +0500

    magick command fixed (234 current)

--- configs/wlogout/wlogout_launch.sh
+++ configs/wlogout/wlogout_launch.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 [ -d ~/.cache/wlogout ] || mkdir -p ~/.cache/wlogout
 grim ~/.cache/wlogout/shot.png
-magick ~/.cache/wlogout/shot.png --blur 0x8 ~/.cache/wlogout/shot.png
+magick ~/.cache/wlogout/shot.png -blur 0x8 ~/.cache/wlogout/shot.png
 wlogout -p layer-shell
