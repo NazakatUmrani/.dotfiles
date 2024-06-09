c95cdf7e4bc39ac8e49a0a63aff28d468ec9dfc2
Nazakat Umrani - NixOS Linux <unazakat70@gmail.com>
2024-06-10 02:37:16 +0500

    wlogout blurred wallpaper dir changed (236 current)

--- configs/wlogout/wlogout_launch.sh
+++ configs/wlogout/wlogout_launch.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-[ -d ~/.cache/wlogout ] || mkdir -p ~/.cache/wlogout
-grim ~/.cache/wlogout/shot.png
-magick ~/.cache/wlogout/shot.png -blur 0x8 ~/.cache/wlogout/shot.png
+grim ~/.config/wlogout/shot.png
+magick ~/.config/wlogout/shot.png -blur 0x8 ~/.config/wlogout/shot.png
 wlogout -p layer-shell
