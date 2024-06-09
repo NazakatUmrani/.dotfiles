84b0d299472d9d8aa2c0cd24500d4071a51f91c0
Nazakat Umrani - NixOS Linux <unazakat70@gmail.com>
2024-06-09 15:29:52 +0500

    Wallpaper directory changed

--- configs/hypr/wallpaperSetter.sh
+++ configs/hypr/wallpaperSetter.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-swww img MyData/Wallpapers/gruvbox-mountain-village.png --transition-step 255 &
+swww img ~/Pictures/Wallpapers/gruvbox-mountain-village.png --transition-step 255 &
 # swww img /home/nazakat/Pictures/Wallpapers/rebellious_tower.png --transition-step 59 --transition-fps 30 --transition-type grow --transition-pos 1285,100 &
