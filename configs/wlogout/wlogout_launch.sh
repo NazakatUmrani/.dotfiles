574bce079ef48861f01979836c4375a396ba2988
Nazakat Umrani - NixOS Linux <unazakat70@gmail.com>
2024-06-12 00:18:26 +0500

    wlogout script modified to load fast, using only wallpaper as background image

--- configs/wlogout/wlogout_launch.sh
+++ configs/wlogout/wlogout_launch.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-grim ~/.config/wlogout/shot.png
-magick ~/.config/wlogout/shot.png -blur 0x8 ~/.config/wlogout/shot.png
-wlogout -p layer-shell
+# grim ~/.config/wlogout/shot.png
+# magick ~/.config/wlogout/shot.png -blur 0x8 ~/.config/wlogout/shot.png
+wlogout
