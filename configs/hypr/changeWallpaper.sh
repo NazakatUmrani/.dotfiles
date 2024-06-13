79bd15746524f62fa72fe6a4e13a7908eaf68335
Nazakat Umrani - NixOS Linux <unazakat70@gmail.com>
2024-06-13 20:56:09 +0500

    wallpaper script issues resolving (250 current)

--- configs/hypr/changeWallpaper.sh
+++ configs/hypr/changeWallpaper.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-wall=$(find ~/Pictures/Wallpapers/ -type f -name "*.jpg" -o -name "*.png" -o -name "*.gif" | shuf -n 1)
-swww img $wall --transition-step 255 &
+wall=$(find ~/Pictures/Wallpapers/ -type f -name "*.jpg" -o -name "*.png" | shuf -n 1)
 
-wal -c
-wal -i $wall
+swww query || swww-daemon --format xrgb && swww img $wall --transition-step 59 --transition-fps 30 --transition-type grow --transition-pos 1285,100
+wal -i $wall -s -t
+# swww img /home/nazakat/Pictures/Wallpapers/rebellious_tower.png --transition-step 59 --transition-fps 30 --transition-type grow --transition-pos 1285,100 &
 
 # updates terminal colors
 # source "$HOME/.cache/wal/colors.sh"
 
 cp ~/.cache/wal/colors-waybar.css ~/.config/waybar/colors-waybar.css
 
-~/.config/waybar/scripts/launch.sh init
+~/.config/waybar/scripts/launch.sh init &
 
-notify-send "Theme and wallpaper updated" "Wallpaper: $wall"
+notify-send "Theme and wallpaper updated" "Wallpaper: $(echo $wall | sed 's|/$HOME/Pictures/Wallpapers/||g')"
 
 
 # swww img /home/nazakat/Pictures/Wallpapers/rebellious_tower.png --transition-step 59 --transition-fps 30 --transition-type grow --transition-pos 1285,100 &
